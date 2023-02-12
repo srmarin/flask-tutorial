5d0a89720a39433572a91fa42cde1be59fed1c48
Sergio Rodriguez <sergiobasket93@gmail.com>
2023-02-12 10:43:37 +0100

    UserSchema modificado

--- schemas.py
+++ schemas.py
@@ -19,7 +19,7 @@ class PlainTagSchema(Schema):
 class UserSchema(Schema):
     id = fields.Str(dump_only=True)
     username = fields.Str(required=True)
-    password = fields.Str(required=True, load_only=True)
+    password = fields.Str(required=True)
 
 
 class ItemUpdateSchema(Schema):
