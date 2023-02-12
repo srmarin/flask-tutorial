8b998c1929d4466235e9fc7c38b198948e16644a
Sergio Rodriguez <sergiobasket93@gmail.com>
2023-02-12 10:42:55 +0100

    se añaden mensajes de error para jwt

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import os
-import secrets
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+
 from db import db
+from blocklist import BLOCKLIST
 import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 
 # Factory pattern
 def create_app(db_url=None):
+
     app = Flask(__name__)
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
@@ -22,21 +26,98 @@ def create_app(db_url=None):
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    openapi_swagger_ui_url = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = openapi_swagger_ui_url
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url \
+        or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
+    migrate = Migrate(app, db)
+
     api = Api(app)
 
-    app.config["JWT_SECRET_KEY"] = "178989100213983495193993022287431830399" #secrets.SystemRandom().getrandbits(128)
+    # secrets.SystemRandom().getrandbits(128)
+    app.config["JWT_SECRET_KEY"] = "178989100213983495193993022287431830399"
     jwt = JWTManager(app)
 
-    with app.app_context():
-        db.create_all()
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token has been revoked.",
+                    "error": "token_revoked"
+                }
+            ),
+            401
+        )
+    
+    @jwt.needs_fresh_token_loader
+    def  token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required"
+                }
+            ),
+            401
+        )
+
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        else:
+            return {"is_admin": False}
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "message": "The token has expired",
+                    "error": "token_expired"
+                }
+            ),
+            401
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "message": "Signature verification failed.",
+                    "error": "invalid_token"
+                }
+            ),
+            401
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "message": "Request does not contain an access token.",
+                    "error": "authorization_required"
+                }
+            ),
+            401
+        )
+
+    # with app.app_context():
+    #     db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
